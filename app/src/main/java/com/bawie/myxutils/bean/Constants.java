26255d8394bb0fa8439ce1ae70fbde2abe743b7c
colorzx0716 <31384907+colorzx0716@users.noreply.github.com>
2017-09-01 19:40:01 +0800

    four

---
+++ app/src/main/java/com/bawie/myxutils/bean/Constants.java
@@ -0,0 +1,8 @@
+package com.bawie.myxutils.bean;
+
+/**
+ * Created by 张肖肖 on 2017/9/1.
+ */
+
+public class Constants {
+}
---
+++ app/src/main/java/com/bawie/myxutils/utils/SharedPreferencesUtil.java
@@ -0,0 +1,8 @@
+package com.bawie.myxutils.utils;
+
+/**
+ * Created by 张肖肖 on 2017/9/1.
+ */
+
+public class SharedPreferencesUtil {
+}

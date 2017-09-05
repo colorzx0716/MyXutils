b121866209c654aefd1d340264b8d60bfa683500
colorzx0716 <31384907+colorzx0716@users.noreply.github.com>
2017-09-05 13:47:31 +0800

    ff

---
+++ app/src/main/java/com/bawie/myxutils/QQActivity.java
@@ -0,0 +1,13 @@
+package com.bawie.myxutils;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QQActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qq);
+    }
+}
